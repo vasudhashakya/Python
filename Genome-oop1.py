4028d83a0a9a339eb6bc90becf8ad84ed7c4a231
vasudha shakya <eeevasudha@yahoo.com>
2016-07-25 16:02:42 -0700

    Small modifications for multiple sequence

--- Genome-oop1.py
+++ Genome-oop1.py
@@ -2,9 +2,11 @@
 
 class Genome:
 	x=0
-	
-	def __init__(self):
-		print "I am constructed"
+	name=""
+	def __init__(self,category):
+		self.name=category
+		print "constructed: in category",self.name
+		
 		
 	def sequence(self):
 		self.x +=1
@@ -12,12 +14,18 @@ class Genome:
 		
 	def __del__(self):
 		print "destructed",self.x
-gs=Genome()
+
+		
+gs=Genome("category1")
 print "\n\n/*****Info on genome class****\\\n\n"
 print "Type",type(gs)
 print "Dir",dir(gs)
 
+gs.sequence()
+gs.sequence()
+gs.sequence()
 
+gs=Genome("category2")
 gs.sequence()
 gs.sequence()
 gs.sequence()
